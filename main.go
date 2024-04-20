f838f11443a6b2fb7e8c09d304db2af20c0cd71f
Tokmyrza <kakenov.tokmyrza@gmail.com>
2024-04-20 10:30:15 +0300

    questions

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
-	//twoSums usage
+	q3answer := removeDuplicates([]int{1, 2, 3, 4, 3, 2, 4, 8, 12, 34})
+	fmt.Println(q3answer)
+
+	q4answer := twoSum([]int{1, 100, 3, 6, 8, 9}, 10)
+	fmt.Println(q4answer)
+
+	fmt.Println("DONE")
 }
 
 func twoSum(nums []int, target int) []int {
@@ -14,4 +22,23 @@ func twoSum(nums []int, target int) []int {
 		temp[n] = i
 	}
 	return []int{}
+}
+
+func removeDuplicates(nums []int) []int {
+	res := []int{}
+	for _, item := range nums {
+		if !contains(res, item) {
+			res = append(res, item)
+		}
+	}
+	return res
+}
+
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
